0170098dd03c28f519eab1b47e803d6322320470
酷酷的辉 <995631664@qq.com>
2024-03-14 14:10:19 +0800

    操作符详解

--- 12_OperatorExample/Program.cs
+++ 12_OperatorExample/Program.cs
@@ -25,6 +25,30 @@ namespace _12_OperatorExamples
             bool m = true;
             bool n = !m;
             Console.WriteLine($"{m}, {n}");
+            
+            // 左移与右移操作符
+            // 在数据位不溢出的情况下, 左移, 数*2; 右移, 数/2
+            // 补位规则:
+            // 左移: 补0
+            // 右移: 是正数: 补0; 是负数: 补1
+            int a = 120;
+            int b = a << 1;  // 左移1位
+            int c = a >> 2;  // 右移1位
+            Console.WriteLine($"a: {a}, b: {b}, c: {c}");
+            Console.WriteLine(Convert.ToString(a, 2).PadLeft(32, '0'));
+            Console.WriteLine(Convert.ToString(b, 2).PadLeft(32, '0'));
+            Console.WriteLine(Convert.ToString(c, 2).PadLeft(32, '0'));
+            
+            // 按位与&   按位或|   按位异或^ 
+            // 条件与&&    条件或||
+            int d = 3;
+            int e = 4;
+            int f = 3;
+            if (d > e && f++ > 3)
+            { 
+                Console.WriteLine("Hello");
+            }
+            Console.WriteLine(f);  // 3, 发生了短路!
         }
     }
 }
---
+++ 14_OperatorExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _14_OperatorExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 可空类型
+            // 默认情况下, int 不能赋值为空
+            // int x = null;
+            
+            // 使用Nullable泛型来定义空类型
+            Nullable<int> x = null;
+            Console.WriteLine(x.HasValue);
+            // 更简洁的语法
+            int? y = null;
+            Console.WriteLine(y.HasValue);
+            
+            // Null值合并操作符
+            int z = y ?? 1;  // 如果y是null, 就赋值1
+            Console.WriteLine(z);
+            
+            // 三目运算符
+            int m = 80;
+            string strT = (m >= 60) ? "Pass" : "Failed";
+            Console.WriteLine(strT); 
+        }
+    }
+}
