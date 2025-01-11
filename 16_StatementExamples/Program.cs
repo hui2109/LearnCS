8cbe4e13f2aa691dde80c1b61843fcf490e7af81
辉哥 <995631664@qq.com>
2025-01-11 21:00:28 +0800

    语句表达式

---
+++ 16_StatementExamples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_StatementExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 标签语句：用一个标签标记一个语句
+            //hello: Console.WriteLine("hello world!");
+            //    goto hello;
+        }
+    }
+}
