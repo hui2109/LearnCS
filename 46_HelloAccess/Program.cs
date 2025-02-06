bc2a6b385b0d8e7f1d56a0c21d36e59987eb0bb3
Zhang_Xuhui <995631664@qq.com>
2025-02-06 10:38:05 +0800

    类与类成员的访问权限

---
+++ 46_HelloAccess/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_HelloAccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Timothy");
+            Console.WriteLine(car.Owner);
+            car.ShowOwner();
+        }
+    }
+
+    class Vehicle
+    {
+        public string Owner { get; set; }
+
+        public Vehicle(string owner)
+        {
+            Owner = owner;
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public Car(string owner) : base(owner)  // 子类要想初始化, 必须先初始化父类, 而父类的构造函数需要一个参数, 因此需要先调用父类的构造函数并传参
+        {
+            // 什么也不做
+        }
+
+        public void ShowOwner()
+        {
+            Console.WriteLine(base.Owner);  // 此Owner属性是继承下来的
+        }
+    }
+}
